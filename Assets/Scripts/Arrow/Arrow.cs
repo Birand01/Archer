d3b384cc9d7dedb9253c8bafb5e3eac606ebfd11
Birand Kılınç <pacmaidanapac@gmail.com>
2024-01-23 14:04:02 +0300

    Arrow system is completed

---
+++ Assets/Scripts/Arrow/Arrow.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject.SpaceFighter;
+
+public class Arrow : MonoBehaviour
+{
+    private Vector3 startPosition, finalLookDirection;
+    private float conquaredDistance;
+    private Rigidbody rb;
+    [SerializeField] private float maxDistance,arrowSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        conquaredDistance = Vector3.Distance(transform.position, startPosition);
+        if (conquaredDistance >= maxDistance)
+        {
+            DisableGameObject();
+        }
+
+    }
+
+    private void DisableGameObject()
+    {
+        rb.velocity = Vector3.zero;
+        this.gameObject.SetActive(false);
+    }
+
+    internal void InitializeArrow(IDamageable damageable)
+    {
+        startPosition = transform.position;
+        Vector3 direction = (damageable.GetTransform().position - startPosition).normalized;      
+        rb.velocity = direction * arrowSpeed;
+    }
+    internal void Aim(IDamageable damageable)
+    {
+
+        var rotationSpeed = arrowSpeed * Time.deltaTime;
+        Vector3 direction = damageable.GetTransform().position - transform.position;
+        finalLookDirection = Vector3.Lerp(finalLookDirection, direction, rotationSpeed);
+        transform.rotation = Quaternion.LookRotation(finalLookDirection);
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Zenject.SpaceFighter;
+
+public class PlayerShoot : MonoBehaviour
+{
+    private CompositeDisposable _disposable = new CompositeDisposable();
+    [SerializeField] protected List<Transform> shootPositions;
+    [SerializeField] protected GameObject arrowPrefab;
+    [SerializeField] protected float reloadDelay;
+
+    private bool canShoot = true;
+    private Collider[] playerColliders;
+    private float currentDelay = 0f;
+
+    protected virtual void Awake()
+    {
+        playerColliders = GetComponentsInParent<Collider>();
+    }
+    private void OnEnable()
+    {
+        AttackRadius.OnAttackEvent += Shoot;
+    }
+    private void OnDisable()
+    {
+        AttackRadius.OnAttackEvent -= Shoot;
+
+        _disposable.Clear();
+    }
+
+    protected virtual void Start()
+    {
+        Observable.EveryUpdate().Subscribe(_ =>
+        {
+            ShootFrequency();
+        }).AddTo(_disposable);
+    }
+    private void ShootFrequency()
+    {
+        if (canShoot == false)
+        {
+            currentDelay -= Time.deltaTime;
+            if (currentDelay <= 0f)
+            {
+                canShoot = true;
+            }
+        }
+    }
+   
+
+
+    private void  Shoot(IDamageable damageable)
+    {
+        if (canShoot)
+        {
+            canShoot = false;
+            currentDelay = reloadDelay;
+            foreach (var barrel in shootPositions)
+            {
+                GameObject arrow = Instantiate(arrowPrefab);
+                arrow.transform.position =new Vector3(barrel.position.x, barrel.position.y+0.5f, barrel.position.z);
+                arrow.transform.rotation = Quaternion.LookRotation(damageable.GetTransform().position);
+                //arrow.GetComponent<Arrow>().Aim(damageable);
+                arrow.GetComponent<Arrow>().InitializeArrow(damageable);
+                foreach (var colliders in playerColliders)
+                {
+                    Physics.IgnoreCollision(arrow.GetComponent<Collider>(), colliders);
+                }
+            }
+        }
+    }
+}
