00a5d47cc8149fe4e0a35dd24f9891f56ee440d9
Birand Kılınç <pacmaidanapac@gmail.com>
2024-01-27 01:02:55 +0300

    Working on Attack range skill button

---
+++ Assets/Scripts/UI/SkillButtons/AttackRange/AttackRangeButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackRangeButton : ButtonBase
+{
+    protected override void OnButtonClickEvent()
+    {
+       
+    }
+}
